1ae900698c569612d4f4434f561f67df007edd12
unknown <gabryelpro260406@gmail.com>
2025-01-05 21:43:27 -0300

    ler caracteres

--- Aulas/Modulo001/M01A07/lendoNumeros.c
+++ Aulas/Modulo001/M01A07/lendoNumeros.c
@@ -3,12 +3,28 @@
 #include <locale.h>
 void main(){
     setlocale(LC_ALL,"Portuguese");
-    int n;
-    float m;
-    printf("Digite um número inteiro: ");
-    scanf("%d", &n);
-    printf("Digite um número real: ");
-    scanf("%f", &m);
-    printf("Você acabou de digitar os valores %d. e %.1f Obrigado\n", n, m);
+
+    char r;
+    char s;
+    printf("Digite só uma letra: ");
+    fflush(stdin);
+    r = getchar();
+    printf("Digite outra letra: ");
+    fflush(stdin);
+    s = getchar();
+    printf("Você digitou a letra \"%c\" e \"%c\"\n", r, s);
+
     system("pause");
 }
+/*
+
+    existe essas duas formas de usar ler uma entrada do teclado
+
+    1. scanf("%c", v);
+    2. v = getchar();
+
+    NOTA: tenho sempre que usar o fflush(stdin); antes de ler.
+    NOTA: caso use o scanf(" %c", v) alguns compiladores aceitam a forma de dar espaço do tipo da variavel
+    eliminando o fflush, embora nem todos aceitem
+
+*/
